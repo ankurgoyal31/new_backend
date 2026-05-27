ad119710ec72fa31694135f86f56335e862a131a
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-05-27 16:04:37 +0530

    Update app.js

--- UB-Admin-EJS/app.js
+++ UB-Admin-EJS/app.js
@@ -53,7 +53,9 @@ app.use(express.urlencoded({
   extended: true,
   limit: "50mb"
 }));
-app.use(express.json(limit: "50mb"));
+app.use(express.json({
+  limit: "50mb"
+}));
 app.use(methodOverride('_method'));
 
 // Static Folder
