c507b703d1f718af9c93ffcd41bec161e173593c
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-09 10:58:42 +0530

    Update blogRoutes.js

--- UB-Admin-EJS/routes/blogRoutes.js
+++ UB-Admin-EJS/routes/blogRoutes.js
@@ -1,13 +1,29 @@
+// const express = require('express');
+// const router = express.Router();
+// const blogController = require('../controllers/blogController');
+// const { isAuthenticated } = require('../middleware/authMiddleware');
+
+// router.get('/', isAuthenticated, blogController.list);
+// router.get('/add', isAuthenticated, blogController.addForm);
+// router.post('/add', isAuthenticated, blogController.create);
+// router.get('/edit/:id', isAuthenticated, blogController.editForm);
+// router.put('/edit/:id', isAuthenticated, blogController.update);
+// router.delete('/delete/:id', isAuthenticated, blogController.delete);
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 const { isAuthenticated } = require('../middleware/authMiddleware');
+const upload = require('../middleware/upload');
 
 router.get('/', isAuthenticated, blogController.list);
 router.get('/add', isAuthenticated, blogController.addForm);
-router.post('/add', isAuthenticated, blogController.create);
+router.post('/add', isAuthenticated, upload.fields([{ name: 'heroImage', maxCount: 1 }, { name: 'main_blog_image', maxCount: 1 }, { name: 'images', maxCount: 10 }, { name: 'project_detail_image_1', maxCount: 1 }, { name: 'project_detail_image_2', maxCount: 1 }]), blogController.create);
 router.get('/edit/:id', isAuthenticated, blogController.editForm);
-router.put('/edit/:id', isAuthenticated, blogController.update);
+router.put('/edit/:id', isAuthenticated, upload.fields([{ name: 'heroImage', maxCount: 1 }, { name: 'main_blog_image', maxCount: 1 }, { name: 'images', maxCount: 10 }, { name: 'project_detail_image_1', maxCount: 1 }, { name: 'project_detail_image_2', maxCount: 1 }]), blogController.update);
 router.delete('/delete/:id', isAuthenticated, blogController.delete);
 
-module.exports = router;
+module.exports = router; 
