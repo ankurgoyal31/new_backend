1b55a668b85d736170077b881bb6f81a89f6be02
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-09 10:46:25 +0530

    Update Blog.js

--- UB-Admin-EJS/models/Blog.js
+++ UB-Admin-EJS/models/Blog.js
@@ -3,6 +3,19 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
   title: String,
   content: String,
+  author: String,
+  category: { 
+    type: String, 
+    enum: ['All', 'Architecture', 'Interior Design', 'Lifestyle', 'Township Planning'], 
+    default: 'All'
+  },
+  heroImage: String,
+  Banner_content: String,
+  main_blog_image: String,
+  project_detail_image_1: String,
+  project_detail_image_2: String,
+  images: [String], 
+
   status: { type: String, default: 'Published' }
 }, { timestamps: true });
 
